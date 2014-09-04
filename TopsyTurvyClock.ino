858c012543163a29c9f64456df96f748cc831153
Andy <andy@workshopshed.com>
2014-09-04 14:32:22 +0100

    Update and rename Clock.ino to TopsyTurvyClock.ino

---
+++ TopsyTurvyClock.ino
@@ -0,0 +1,51 @@
+#include "CustomStepper.h"  //http://playground.arduino.cc/Main/CustomStepper
+#include "Delay.h"
+#include "Controller.h"
+#include "Timezone.h"  //https://github.com/JChristensen/Timezone
+#include "Clock.h"     //move to controller.h?
+#include "DS1307RTC.h" //move to clock ? http://www.pjrc.com/teensy/td_libs_DS1307RTC.html
+
+CustomStepper stepper1(15,14,16,10,18,19, (byte[]){8, B1000, B1100, B0100, B0110, B0010, B0011, B0001, B1001}, 4075.7728395, 12, CW);
+CustomStepper stepper2(6,7,8,20,5,4, (byte[]){8, B1000, B1100, B0100, B0110, B0010, B0011, B0001, B1001}, 4075.7728395, 12, CW);
+
+TimeChangeRule BST = {"BST", Last, Sun, Mar, 1, 60}; //British Summer Time
+TimeChangeRule GMT = {"GMT", Last, Sun, Oct, 2, 0};  //Standard Time
+Timezone tz(BST, GMT);
+
+Clock clock(tz);
+
+Controller con(stepper1,stepper2,clock;
+
+NonBlockDelay d;
+bool motorsinit;
+
+void setup()
+{
+  Serial.begin(9600);
+  while (!Serial) {
+  ; // wait for serial port to connect. Needed for Leonardo only
+  }
+  
+  con.init();
+}
+
+void loop()
+{
+  con.readinput();
+  con.run();
+  
+  if (d.Timeout()) {
+        con.dump();
+  d.Delay(2000);
+  }  
+  
+
+}
+
+
+
+
+
+
+
+
